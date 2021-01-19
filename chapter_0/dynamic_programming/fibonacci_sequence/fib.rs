0fd6ffd6a3204ac6d36b4ab2a7c0b94f628b4527
Mengsen <mengsen_wang@163.com>
2021-01-19 19:47:41 +0800

    2021-1-19 19:47:38

--- chapter_0/dynamic_programming/fibonacci_sequence/fib.rs
+++ chapter_0/dynamic_programming/fibonacci_sequence/fib.rs
@@ -1,3 +1,10 @@
+/*
+ * @Author: Mengsen.Wang
+ * @Date: 2021-01-19 09:46:53
+ * @Last Modified by:   Mengsen.Wang
+ * @Last Modified time: 2021-01-19 09:46:53
+ */
+
 fn just_recursive(n: i32) -> i32 {
     if n == 1 || n == 2 {
         return 1;
@@ -19,8 +26,7 @@ fn fib(n: i32) -> i32 {
     (0..n).fold((0, 1), |(pp, p), _| (p, pp + p)).0
 }
 
-
-fn main () {
+fn main() {
     println!("The result = {}", just_recursive(45));
     println!("The result = {}", dp_fib(45));
     println!("The result = {}", fib(45));
