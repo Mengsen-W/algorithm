bc2faa7762c09ac76af67f9ab2b33277cbbfb2e3
mengsen <mengsen_wang@163.com>
2021-01-22 23:20:52 +0800

    debug

--- chapter_0/backtracking_algorithm/N-Queue/n_queue.rs
+++ chapter_0/backtracking_algorithm/N-Queue/n_queue.rs
@@ -6,41 +6,72 @@
  */
 
 fn solve_n_queens(n: i32) -> Vec<Vec<String>> {
-    let mut res: Vec<Vec<char>> = vec![];
-    let mut board: Vec<char> = vec![vec!['.', n]; n];
-    backtrace(board, 0, res);
+    let mut res: Vec<Vec<String>> = vec![];
+    let mut board: Vec<String> =
+        vec![vec!['.'; n as usize].into_iter().collect::<String>(); n as usize];
+    back_trace(&board, 0, &res);
     res
 }
 
-fn backtrace(mut board: &Vec<char>, row: i32, mut res: &Vec<Vec<char>>) {
-    if row == board.len() {
-        res.push(board);
+fn back_trace(mut board: &Vec<String>, row: i32, mut res: &Vec<Vec<String>>) {
+    if row == board.len() as i32 {
+        res.push(*board);
         return;
         // return true;
     }
 
-    for col in 0..board[row].len() {
-        if !isVaild(board, row, col) {
+    for col in 0..(*board[row as usize]).len() {
+        if !is_valid(board, row, col as i32) {
             continue;
         }
         // 做选择
-        board[row][col] = 'Q';
+        board[row as usize].chars().nth(col as usize).unwrap() = 'Q';
         // 进入下一行决策
-        backtrack(board, row + 1);
+        back_trace(board, row + 1, res);
         // if backtrack(board, row + 1) {return true;}
         // 撤销选择
-        board[row][col] = '.';
+        board[row as usize].chars().nth(col as usize).unwrap() = '.';
     }
     // return false;
 }
 
-fn is_vaild(board: &Vec<char>, row: i32, col: i32) -> bool {
+fn is_valid(board: &Vec<String>, row: i32, col: i32) -> bool {
     for i in 0..board.len() {
-        if board[i][col] == 'Q' {
+        if board[i as usize].chars().nth(col as usize).unwrap() == 'Q' {
             return false;
         }
     }
-    //TODO
+
+    let mut i: i32 = row - 1;
+    let mut j: i32 = col + 1;
+    while i >= 0 && j < board.len() as i32 {
+        if board[i as usize].chars().nth(j as usize).unwrap() == 'Q' {
+            return false;
+        }
+        i -= 1;
+        j += 1;
+    }
+
+    let mut i: i32 = row - 1;
+    let mut j: i32 = col - 1;
+    while i >= 0 && j >= 0 {
+        if board[i as usize].chars().nth(j as usize).unwrap() == 'Q' {
+            return false;
+        }
+    }
+
+    true
 }
 
-fn main() {}
+fn main() {
+    let res = solve_n_queens(4);
+}
+
+#[test]
+fn test() {
+    let s: String = "Hello".to_string();
+    println!("The result = {:?}", s.chars().nth(3).unwrap());
+    println!("The result = {:?}", s.get(2..3).unwrap());
+    let s: String = vec!['.'; 10].into_iter().collect::<String>();
+    println!("The result = {:?}", s);
+}
