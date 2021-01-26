37a7eca435bc0b24838d4b3a13fbbbffc5c94743
Mengsen <mengsen_wang@163.com>
2021-01-26 20:31:53 +0800

    2021-01-26 20:31:49

--- chapter_0/bfs/open_lock/open_lock.rs
+++ chapter_0/bfs/open_lock/open_lock.rs
@@ -5,4 +5,14 @@
  * @Last Modified time: 2021-01-24 22:51:16
  */
 
-fn open_lock(deadends: Vec<String>, target: String) -> i32 {}
+// fn open_lock(deadends: Vec<String>, target: String) -> i32 {}
+
+// fn open_lock_binary(deadends: Vec<String>, target: String) -> i32 {}
+
+fn main() {
+    let mut s: String = "Hello Rust".to_string();
+    let mut b = s.into_bytes();
+    b[1] = b'Q';
+    s = String::from_utf8(b).unwrap();
+    println!("{}", s);
+}
