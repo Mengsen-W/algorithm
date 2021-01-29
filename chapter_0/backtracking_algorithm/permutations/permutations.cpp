ffe98d5da058c3a879a4cf548b1e29f8b560bec4
Mengsen Wang <mengsen_wang@163.com>
2021-01-29 19:14:08 +0800

    2021-01-29 19:14:302

--- chapter_0/backtracking_algorithm/permutations/permutations.cpp
+++ chapter_0/backtracking_algorithm/permutations/permutations.cpp
@@ -2,14 +2,15 @@
  * @Author: Mengsen.Wang
  * @Date: 2021-01-19 17:16:24
  * @Last Modified by: Mengsen.Wang
- * @Last Modified time: 2021-01-19 18:57:36
+ * @Last Modified time: 2021-01-29 19:13:14
  */
 
 #include <algorithm>
 #include <functional>
+#include <iostream>
 #include <vector>
 
-std::vector<std::vector<int>>& premute(std::vector<int>& nums) {
+std::vector<std::vector<int>> premute(std::vector<int>& nums) {
   // 记录结果
   std::vector<std::vector<int>> res{};
   // 记录回溯过程中的状态
@@ -25,7 +26,7 @@ std::vector<std::vector<int>>& premute(std::vector<int>& nums) {
     }
 
     for (int i = 0; i < nums.size(); ++i) {
-      if (trace.end() == std::find(trace.begin(), trace.end(), nums[i]))
+      if (trace.end() != std::find(trace.begin(), trace.end(), nums[i]))
         continue;
       trace.push_back(nums[i]);
       backtrace();
@@ -38,4 +39,17 @@ std::vector<std::vector<int>>& premute(std::vector<int>& nums) {
   return res;
 }
 
-int main() { return 0; }
+int main() {
+  std::vector<int> nums = {1, 2, 3, 4, 5};
+  std::vector<std::vector<int>> res = premute(nums);
+
+  std::cout << "number = " << res.size() << std::endl;
+
+  for (int i = 0; i < res.size(); ++i) {
+    for (int j = 0; j < res[i].size(); ++j) {
+      std::cout << res[i][j];
+    }
+    std::cout << std::endl;
+  }
+  return 0;
+}
---
+++ chapter_0/max_profit/max_profit.cpp
@@ -0,0 +1,6 @@
+/*
+ * @Author: Mengsen.Wang
+ * @Date: 2021-01-29 18:50:01
+ * @Last Modified by: Mengsen.Wang
+ * @Last Modified time: 2021-01-29 18:50:21
+ */
