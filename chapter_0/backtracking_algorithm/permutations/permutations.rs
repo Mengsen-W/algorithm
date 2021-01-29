ae213507c90bd72dcb2f12c716fa93e059bdf017
Mengsen <mengsen_wang@163.com>
2021-01-29 19:14:08 +0800

    2021-01-29 19:14:302

--- chapter_0/backtracking_algorithm/permutations/permutations.rs
+++ chapter_0/backtracking_algorithm/permutations/permutations.rs
@@ -2,32 +2,40 @@
  * @Author: Mengsen.Wang
  * @Date: 2021-01-19 18:29:40
  * @Last Modified by: Mengsen.Wang
- * @Last Modified time: 2021-01-28 20:29:39
+ * @Last Modified time: 2021-01-29 19:13:35
  */
 
-// TODO
-fn premute(&nums: Vec<i32>) -> &Vec<Vec<i32>> {
+fn premute(nums: &Vec<i32>) -> Vec<Vec<i32>> {
     // 记录结果
-    let res: Vec<Vec<i32>> = vec![];
+    let mut res: Vec<Vec<i32>> = vec![];
     // 记录追踪
     let mut trace: Vec<i32> = vec![];
 
-    fn back_trace(&nums: Vec<i32>, trace: &mut Vec<i32>, res: &mut Vec<Vec<i32>>) {
+    fn back_trace(nums: &Vec<i32>, trace: &mut Vec<i32>, res: &mut Vec<Vec<i32>>) {
         if nums.len() == trace.len() {
-            res.push(trace);
+            res.push(trace.to_vec());
             return;
         }
 
         for i in 0..nums.len() {
-          if trace.iter().all(|&x|, x == nums[i]) continue;
-          trace.push(nums[i]);
-          backtrace(nums, trace, res);
-          trace.pop();
+            if trace.iter().any(|&x| x == nums[i]) {
+                continue;
+            }
+            trace.push(nums[i]);
+            back_trace(nums, trace, res);
+            trace.pop();
         }
     }
 
-    backtrace(nums, trace, res);
+    back_trace(nums, &mut trace, &mut res);
     return res;
 }
 
-fn main() {}
+fn main() {
+    let nums: Vec<i32> = vec![1, 2, 3, 4, 5];
+    let res = premute(&nums);
+    println!("number = {}", res.len());
+    for v in res {
+        println!("{:?}", v);
+    }
+}
---
+++ chapter_0/max_profit/max_profit.rs
@@ -0,0 +1,6 @@
+/*
+ * @Author: Mengsen.Wang
+ * @Date: 2021-01-29 18:50:13
+ * @Last Modified by: Mengsen.Wang
+ * @Last Modified time: 2021-01-29 18:52:13
+ */
