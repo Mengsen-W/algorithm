6514b927b0ae335e775619b8ecf09fd954bc5116
Mengsen Wang <mengsen_wang@163.com>
2021-02-03 19:01:33 +0800

    2021-02-03 19:01:27

--- chapter_0/max_profit/max_profit.cpp
+++ chapter_0/max_profit/max_profit.cpp
@@ -24,10 +24,37 @@ dp[i][0][1] = -INT_MAX
 不允许交易，还持有股票，表示不可能状态
 */
 
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
-int max_profit_k_1(vector<int> prices) {}
+// dp[i][1][0] = max (dp[i-1][1][0], dp[i-1][1][1] + prices[i])
+// dp[i][1][1] = max (dp[i-1][1][1], dp[i-1][0][0] - prices[i])
+//             = max (dp[i-1][1][1], -prices[i]
+// dp[i][0] = max (dp[i-1][0], dp[i-1][1] + prices[i])
+// dp[i][1] = max (dp[i-1][1], -prices[i])
+int max_profit_k_1(vector<int> prices) {
+  {
+    int n = prices.size();
+    vector<vector<int>> dp = (n, (2, 0));
+    dp[0][0] = 0;
+    dp[0][1] = max(INT_MIN - prices[0]);
+    for (int i = 1; i < n; ++i) {
+      dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+      dp[i][1] = max(dp[i - 1][1], -prices[i]);
+    }
+    cout << dp[n - 1][0] << endl;
+  }
+  {
+    int n = prices.size();
+    int dp_i_0 = 0, dp_i_1 = INT_MIN;
+    for (int i = 0; i < n; ++i) {
+      dp_i_0 = max(dp_i_0, dp_i_1 + prices[i]);
+      dp_i_1 = max(dp_i_1, -prices[i]);
+    }
+    cout << dp_i_0 << endl;
+  }
+}
 
 int main() {}
