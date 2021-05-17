63915d55cb0524fad1e01626510e335508421aa9
Mengsen_W <mengsen_wang@163.com>
2021-05-17 09:08:09 +0800

    20210517

---
+++ 993_is_cousins/is_cousins.cpp
@@ -0,0 +1,112 @@
+/*
+ * @Date: 2021-05-17 08:36:11
+ * @Author: Mengsen Wang
+ * @LastEditors: Mengsen Wang
+ * @LastEditTime: 2021-05-17 08:44:34
+ */
+
+#include <cassert>
+#include <queue>
+#include <unordered_map>
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+ public:
+  using PTT = pair<TreeNode *, TreeNode *>;
+  bool isCousins(TreeNode *root, int x, int y) {
+    // 使用队列q来进行bfs
+    // 其中pair中，p.first 记录当前结点的指针，p.second
+    // 记录当前结点的父结点的指针
+    queue<PTT> q;
+    q.push({root, nullptr});
+    while (!q.empty()) {
+      int n = q.size();
+      vector<TreeNode *> rec_parent;
+      for (int i = 0; i < n; i++) {
+        auto [cur, parent] = q.front();
+        q.pop();
+        if (cur->val == x || cur->val == y) rec_parent.push_back(parent);
+        if (cur->left) q.push({cur->left, cur});
+        if (cur->right) q.push({cur->right, cur});
+      }
+      // `x` 和 `y` 都没出现
+      if (rec_parent.size() == 0) continue;
+      // `x` 和 `y` 只出现一个
+      else if (rec_parent.size() == 1)
+        return false;
+      // `x` 和 `y` 都出现了
+      else if (rec_parent.size() == 2)
+        // `x` 和 `y` 父节点 相同/不相同 ？
+        return rec_parent[0] != rec_parent[1];
+    }
+    return false;
+  }
+};
+
+class Solution1 {
+ public:
+  bool isCousins(TreeNode *root, int x, int y) {
+    assert(root && "Unexpected empty tree!");
+    dfs(root, x, y, 0);
+    return (child2Parent[x] != child2Parent[y] &&
+            node2level[x] == node2level[y]);
+  }
+
+ private:
+  unordered_map<int, int> child2Parent;
+  unordered_map<int, int> node2level;
+
+  void dfs(TreeNode *root, int x, int y, int level) {
+    if (!root) {
+      return;
+    }
+
+    if (root->left) {
+      child2Parent[root->left->val] = root->val;
+      node2level[root->left->val] = level + 1;
+    }
+
+    if (root->right) {
+      child2Parent[root->right->val] = root->val;
+      node2level[root->right->val] = level + 1;
+    }
+
+    dfs(root->left, x, y, level + 1);
+    dfs(root->right, x, y, level + 1);
+  }
+};
+
+int main() {
+  {
+    TreeNode *root = new TreeNode{
+        1, new TreeNode{2, new TreeNode{4, nullptr, nullptr}, nullptr},
+        new TreeNode{3, nullptr, nullptr}};
+    assert(!Solution{}.isCousins(root, 4, 3));
+    assert(!Solution1{}.isCousins(root, 4, 3));
+  }
+  {
+    TreeNode *root = new TreeNode{
+        1, new TreeNode{2, nullptr, new TreeNode{4, nullptr, nullptr}},
+        new TreeNode{3, nullptr, new TreeNode{5, nullptr, nullptr}}};
+    assert(Solution{}.isCousins(root, 5, 4));
+    assert(Solution1{}.isCousins(root, 5, 4));
+  }
+  {
+    TreeNode *root = new TreeNode{
+        1, new TreeNode{2, nullptr, new TreeNode{4, nullptr, nullptr}},
+        new TreeNode{3, nullptr, nullptr}};
+    assert(!Solution{}.isCousins(root, 2, 3));
+    assert(!Solution1{}.isCousins(root, 2, 3));
+  }
+}
---
+++ 993_is_cousins/is_cousion.rs
@@ -0,0 +1,164 @@
+/*
+ * @Date: 2021-05-17 08:36:19
+ * @Author: Mengsen Wang
+ * @LastEditors: Mengsen Wang
+ * @LastEditTime: 2021-05-17 09:07:23
+ */
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+fn is_cousins_bfs(root: Option<Rc<RefCell<TreeNode>>>, x: i32, y: i32) -> bool {
+    // Passed 0ms 2mb
+    let mut que = std::collections::VecDeque::new();
+    if root.is_some() {
+        que.push_back(root);
+    }
+    while !que.is_empty() {
+        let mut found = Vec::new();
+        for _ in 0..que.len() {
+            if let Some(node) = que.pop_front() {
+                if let Some(node) = node {
+                    let left = node.borrow_mut().left.take();
+                    let right = node.borrow_mut().right.take();
+                    let parent_val = node.borrow().val;
+                    if left.is_some() {
+                        let val = left.as_ref().unwrap().borrow().val;
+                        if val == x || val == y {
+                            found.push(parent_val);
+                        }
+                        que.push_back(left);
+                    }
+                    if right.is_some() {
+                        let val = right.as_ref().unwrap().borrow().val;
+                        if val == x || val == y {
+                            found.push(parent_val);
+                        }
+                        que.push_back(right);
+                    }
+                }
+            }
+        }
+        if !found.is_empty() {
+            return if found.len() == 1 {
+                false
+            } else {
+                found[0] != found[1]
+            };
+        }
+    }
+    false
+}
+
+fn is_cousins_dfs(root: Option<Rc<RefCell<TreeNode>>>, x: i32, y: i32) -> bool {
+    type Tree = Option<Rc<RefCell<TreeNode>>>;
+    fn find(root: Tree, x: i32, p: Tree, l: i32) -> Option<(Tree, i32)> {
+        let l1 = l + 1;
+        if let Some(node) = &root {
+            let node = node.borrow();
+            if node.val == x {
+                return Some((p, l1));
+            } else {
+                if let Some(r) = find(node.left.clone(), x, root.clone(), l1) {
+                    return Some(r);
+                }
+                if let Some(r) = find(node.right.clone(), x, root.clone(), l1) {
+                    return Some(r);
+                }
+            }
+        }
+        None
+    }
+
+    let (f1, l1) = find(root.clone(), x, None, 0).unwrap();
+    let (f2, l2) = find(root.clone(), y, None, 0).unwrap();
+    f1 != f2 && l1 == l2
+}
+
+fn main() {
+    {
+        let root = Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 4,
+                    left: None,
+                    right: None,
+                }))),
+                right: None,
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 3,
+                left: None,
+                right: None,
+            }))),
+        })));
+        assert!(!is_cousins_bfs(root.clone(), 4, 3));
+        // assert!(!is_cousins_dfs(root.clone(), 4, 3));
+    }
+    {
+        let root = Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: None,
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 4,
+                    left: None,
+                    right: None,
+                }))),
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 3,
+                left: None,
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 5,
+                    left: None,
+                    right: None,
+                }))),
+            }))),
+        })));
+        assert!(is_cousins_bfs(root.clone(), 5, 4));
+        // assert!(is_cousins_dfs(root.clone(), 5, 4));
+    }
+    {
+        let root = Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: None,
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 4,
+                    left: None,
+                    right: None,
+                }))),
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 3,
+                left: None,
+                right: None,
+            }))),
+        })));
+        assert!(!is_cousins_bfs(root.clone(), 2, 3));
+        // assert!(!is_cousins_dfs(root.clone(), 4, 3));
+    }
+}
