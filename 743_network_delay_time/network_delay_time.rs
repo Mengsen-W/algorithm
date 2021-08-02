36950b10316c9cd0805c7cb461aa59fa8efa77bb
Mengsen Wang <mengsen_wang@163.com>
2021-08-02 16:07:16 +0800

    20210802

---
+++ 743_network_delay_time/network_delay_time.rs
@@ -0,0 +1,108 @@
+/*
+ * @Date: 2021-08-02 15:26:27
+ * @Author: Mengsen Wang
+ * @LastEditors: Mengsen Wang
+ * @LastEditTime: 2021-08-02 16:05:11
+ */
+
+pub fn network_delay_time_dijkstra(times: Vec<Vec<i32>>, n: i32, k: i32) -> i32 {
+    let n = n as usize;
+    let k = k as usize;
+    let mut arcs: Vec<Vec<i32>> = vec![vec![10001; n + 1]; n + 1];
+    for i in 1..=n {
+        arcs[i][i] = 0;
+    }
+
+    for time in times.iter() {
+        arcs[time[0] as usize][time[1] as usize] = time[2];
+    }
+    let mut dist = arcs[k].clone();
+    let mut flag = vec![false; n + 1];
+    flag[k] = true;
+    for _ in 1..=n {
+        let mut minn = 10010;
+        let mut pos = 0;
+        for j in 1..=n {
+            if !flag[j] && dist[j] < minn {
+                pos = j;
+                minn = dist[j];
+            }
+        }
+        flag[pos] = true;
+        for j in 1..=n {
+            if !flag[j] && dist[pos] + arcs[pos][j] < dist[j] {
+                dist[j] = dist[pos] + arcs[pos][j];
+            }
+        }
+    }
+    let mut ret = i32::MIN;
+    for i in 1..dist.len() {
+        if dist[i] > ret {
+            ret = dist[i]
+        }
+    }
+    match ret == 10001 {
+        true => -1,
+        false => ret,
+    }
+}
+
+pub fn network_delay_time_floyd(times: Vec<Vec<i32>>, n: i32, k: i32) -> i32 {
+    let n = n as usize;
+    let k = k as usize;
+    let mut arcs: Vec<Vec<i32>> = vec![vec![10010; n + 1]; n + 1];
+    for i in 1..=n {
+        arcs[i][i] = 0;
+    }
+
+    for time in times.iter() {
+        arcs[time[0] as usize][time[1] as usize] = time[2];
+    }
+
+    for r in 1..=n {
+        for i in 1..=n {
+            for j in 1..=n {
+                if arcs[i][j] > arcs[i][r] + arcs[r][j] {
+                    arcs[i][j] = arcs[i][r] + arcs[r][j];
+                }
+            }
+        }
+    }
+    let mut ret = i32::MIN;
+    for i in 1..arcs[k].len() {
+        if arcs[k][i] > ret {
+            ret = arcs[k][i]
+        }
+    }
+    match ret == 10010 {
+        true => -1,
+        false => ret,
+    }
+}
+
+fn main() {
+    {
+        let times = vec![vec![2, 1, 1], vec![2, 3, 1], vec![3, 4, 1]];
+        let n = 4;
+        let k = 2;
+        let ans = 2;
+        assert_eq!(network_delay_time_dijkstra(times.clone(), n, k), ans);
+        assert_eq!(network_delay_time_floyd(times.clone(), n, k), ans);
+    }
+    {
+        let times = vec![vec![1, 2, 1]];
+        let n = 2;
+        let k = 1;
+        let ans = 1;
+        assert_eq!(network_delay_time_dijkstra(times.clone(), n, k), ans);
+        assert_eq!(network_delay_time_floyd(times.clone(), n, k), ans);
+    }
+    {
+        let times = vec![vec![1, 2, 1]];
+        let n = 2;
+        let k = 2;
+        let ans = -1;
+        assert_eq!(network_delay_time_dijkstra(times.clone(), n, k), ans);
+        assert_eq!(network_delay_time_floyd(times.clone(), n, k), ans);
+    }
+}
