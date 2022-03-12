69f8d42d1007e5c71b0570b41fcc2ed6bc95ca10
mengsenwang <mengsen_wang@163.com>
2022-03-12 03:05:58 +0000

    2022年3月12日

---
+++ 590_ntree_postorder/ntree_postorder.cpp
@@ -0,0 +1,75 @@
+/*
+ * @Date: 2022-03-12 02:01:09
+ * @Author: Mengsen Wang
+ * @LastEditors: Mengsen Wang
+ * @LastEditTime: 2022-03-12 02:12:01
+ * @FilePath: /algorithm/590_ntree_postorder/ntree_postorder.cpp
+ */
+
+#include <cassert>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+// Definition for a Node.
+class Node {
+ public:
+  int val;
+  vector<Node *> children;
+
+  Node() {}
+
+  Node(int _val) { val = _val; }
+
+  Node(int _val, vector<Node *> _children) {
+    val = _val;
+    children = _children;
+  }
+};
+
+class Solution {
+ public:
+  vector<int> postorder(Node *root) {
+    vector<int> res;
+    if (root == nullptr) {
+      return res;
+    }
+
+    stack<Node *> st;
+    st.emplace(root);
+    while (!st.empty()) {
+      Node *node = st.top();
+      st.pop();
+      res.emplace_back(node->val);
+      for (auto &item : node->children) {
+        st.emplace(item);
+      }
+    }
+    reverse(res.begin(), res.end());
+    return res;
+  }
+};
+
+int main() {
+  {
+    Node *root = new Node(1);
+    root->children = {new Node(3), new Node(2), new Node(4)};
+    root->children[0]->children = {new Node(5), new Node(6)};
+    assert((Solution().postorder(root) == vector<int>{5, 6, 3, 2, 4, 1}));
+  }
+  {
+    Node *root = new Node{1};
+    vector<Node *> child{new Node{2}, new Node{3}, new Node{4}, new Node{5}};
+    child[1]->children = {new Node{6}, new Node{7}};
+    child[2]->children = {new Node{8}};
+    child[3]->children = {new Node{9}, new Node{10}};
+    child[1]->children[1]->children = {new Node{11}};
+    child[1]->children[1]->children[0]->children = {new Node{14}};
+    child[2]->children[0]->children = {new Node{12}};
+    child[3]->children[0]->children = {new Node{13}};
+    root->children = child;
+    assert((Solution().postorder(root) ==
+            vector<int>{2, 6, 14, 11, 7, 3, 12, 8, 4, 13, 9, 10, 5, 1}));
+  }
+}
---
+++ 590_ntree_postorder/ntree_postorder.go
@@ -0,0 +1,63 @@
+/*
+ * @Date: 2022-03-12 02:01:14
+ * @Author: Mengsen Wang
+ * @LastEditors: Mengsen Wang
+ * @LastEditTime: 2022-03-12 02:22:23
+ * @FilePath: /algorithm/590_ntree_postorder/ntree_postorder.go
+ */
+
+package main
+
+import "reflect"
+
+type Node struct {
+	Val      int
+	Children []*Node
+}
+
+func postorder(root *Node) (ans []int) {
+	if root == nil {
+		return
+	}
+	st := []*Node{root}
+	for len(st) > 0 {
+		node := st[len(st)-1]
+		st = st[:len(st)-1]
+		ans = append(ans, node.Val)
+		st = append(st, node.Children...)
+	}
+	for i, n := 0, len(ans); i < n/2; i++ {
+		ans[i], ans[n-1-i] = ans[n-1-i], ans[i]
+	}
+	return
+}
+
+func main() {
+	assert := func(a, b []int) {
+		if !reflect.DeepEqual(a, b) {
+			panic("Not Passed")
+		}
+	}
+
+	{
+		root := &Node{Val: 1}
+		children := []*Node{{Val: 3}, {Val: 2}, {Val: 4}}
+		children[0].Children = []*Node{{Val: 5}, {Val: 6}}
+		root.Children = children
+		assert(postorder(root), []int{5, 6, 3, 2, 4, 1})
+	}
+
+	{
+		root := &Node{Val: 1}
+		child := []*Node{{Val: 2}, {Val: 3}, {Val: 4}, {Val: 5}}
+		child[1].Children = []*Node{{Val: 6}, {Val: 7}}
+		child[2].Children = []*Node{{Val: 8}}
+		child[3].Children = []*Node{{Val: 9}, {Val: 10}}
+		child[1].Children[1].Children = []*Node{{Val: 11}}
+		child[1].Children[1].Children[0].Children = []*Node{{Val: 14}}
+		child[2].Children[0].Children = []*Node{{Val: 12}}
+		child[3].Children[0].Children = []*Node{{Val: 13}}
+		root.Children = child
+		assert(postorder(root), []int{2, 6, 14, 11, 7, 3, 12, 8, 4, 13, 9, 10, 5, 1})
+	}
+}
