ae864478e3556d7ac107cf4d44a1e6844aacc44d
mengsenwang <mengsen_wang@163.com>
2022-03-24 00:13:47 +0000

    2022年3月24日

---
+++ 661_image_smoother/image_smoother.rs
@@ -0,0 +1,46 @@
+/*
+ * @Date: 2022-03-23 23:29:24
+ * @Author: Mengsen Wang
+ * @LastEditors: Mengsen Wang
+ * @LastEditTime: 2022-03-23 23:59:33
+ * @FilePath: /algorithm/661_image_smoother/image_smoother.rs
+ */
+
+pub fn image_smoother(m: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let mut ret = vec![vec![0; m[0].len()]; m.len()];
+
+    for i in 0..m.len() {
+        for j in 0..m[0].len() {
+            let mut cnt = 0;
+
+            for k in i.saturating_sub(1)..=(i + 1).min(m.len() - 1) {
+                for l in j.saturating_sub(1)..=(j + 1).min(m[0].len() - 1) {
+                    ret[i][j] += m[k][l];
+                    cnt += 1;
+                }
+            }
+
+            ret[i][j] /= cnt;
+        }
+    }
+    ret
+}
+
+fn main() {
+    assert_eq!(
+        image_smoother(vec![vec![1, 1, 1], vec![1, 0, 1], vec![1, 1, 1]]),
+        vec![vec![0, 0, 0], vec![0, 0, 0], vec![0, 0, 0]]
+    );
+    assert_eq!(
+        image_smoother(vec![
+            vec![100, 200, 100],
+            vec![200, 50, 200],
+            vec![100, 200, 100]
+        ]),
+        vec![
+            vec![137, 141, 137],
+            vec![141, 138, 141],
+            vec![137, 141, 137]
+        ]
+    );
+}
