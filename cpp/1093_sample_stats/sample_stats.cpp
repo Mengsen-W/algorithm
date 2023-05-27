e38799b66f8c3b7e6e695d45cfe3429e86fdaa7d
mengsenwang <854284842@qq.com>
2023-05-27 23:30:29 +0800

    2023年5月27日

---
+++ cpp/1093_sample_stats/sample_stats.cpp
@@ -0,0 +1,82 @@
+/*
+ * @Date: 2023-05-27
+ * @LastEditors: 854284842@qq.com
+ * @LastEditTime: 2023-05-27
+ * @FilePath: /algorithm/cpp/1093_sample_stats/sample_stats.cpp
+ */
+
+#include <cassert>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  vector<double> sampleStats(vector<int>& count) {
+    int n = count.size();
+    int total = accumulate(count.begin(), count.end(), 0);
+    double mean = 0.0;
+    double median = 0.0;
+    int minnum = 256;
+    int maxnum = 0;
+    int mode = 0;
+
+    int left = (total + 1) / 2;
+    int right = (total + 2) / 2;
+    int cnt = 0;
+    int maxfreq = 0;
+    long long sum = 0;
+    for (int i = 0; i < n; i++) {
+      sum += (long long)count[i] * i;
+      if (count[i] > maxfreq) {
+        maxfreq = count[i];
+        mode = i;
+      }
+      if (count[i] > 0) {
+        if (minnum == 256) {
+          minnum = i;
+        }
+        maxnum = i;
+      }
+      if (cnt < right && cnt + count[i] >= right) {
+        median += i;
+      }
+      if (cnt < left && cnt + count[i] >= left) {
+        median += i;
+      }
+      cnt += count[i];
+    }
+    mean = (double)sum / total;
+    median = median / 2.0;
+    return {(double)minnum, (double)maxnum, mean, median, (double)mode};
+  }
+};
+
+int main() {
+  {
+    vector<int> count{0, 1, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    vector<double> ans{1.00000, 3.00000, 2.37500, 2.50000, 3.00000};
+    assert(Solution().sampleStats(count) == ans);
+  }
+
+  {
+    vector<int> count{0, 4, 3, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    vector<double> ans{1.00000, 4.00000, 2.18181818181818181818181818182, 2.00000, 1.00000};
+    assert(Solution().sampleStats(count) == ans);
+  }
+}
