bc18d7eb212b38e9ef4500685fce5e30701722d1
mengsen wang <854284842@qq.com>
2024-09-16 19:12:57 +0800

    2024年9月16日

--- rust/1184_distance_between_bus_stops/distance_between_bus_stops.rs
+++ rust/1184_distance_between_bus_stops/distance_between_bus_stops.rs
@@ -4,16 +4,19 @@
  * @LastEditTime: 2022-07-24
  * @FilePath: /algorithm/1184_distance_between_bus_stops/distance_between_bus_stops.rs
  */
+struct Solution;
 
-pub fn distance_between_bus_stops(distance: Vec<i32>, start: i32, destination: i32) -> i32 {
-    if start > destination {
-        return distance_between_bus_stops(distance, destination, start);
+impl Solution {
+    pub fn distance_between_bus_stops(distance: Vec<i32>, start: i32, destination: i32) -> i32 {
+        if start > destination {
+            return Self::distance_between_bus_stops(distance, destination, start);
+        }
+        let (start, end) = (start as usize, destination as usize);
+        (&distance[start..end]).iter().sum::<i32>().min(
+            (&distance[0..start]).iter().sum::<i32>()
+                + (&distance[end..distance.len()]).iter().sum::<i32>(),
+        )
     }
-    let (start, end) = (start as usize, destination as usize);
-    (&distance[start..end]).iter().sum::<i32>().min(
-        (&distance[0..start]).iter().sum::<i32>()
-            + (&distance[end..distance.len()]).iter().sum::<i32>(),
-    )
 }
 
 fn main() {
@@ -21,20 +24,29 @@ fn main() {
         let distance = vec![1, 2, 3, 4];
         let start = 0;
         let destination = 1;
-        assert_eq!(distance_between_bus_stops(distance, start, destination), 1);
+        assert_eq!(
+            Solution::distance_between_bus_stops(distance, start, destination),
+            1
+        );
     }
 
     {
         let distance = vec![1, 2, 3, 4];
         let start = 0;
         let destination = 2;
-        assert_eq!(distance_between_bus_stops(distance, start, destination), 3);
+        assert_eq!(
+            Solution::distance_between_bus_stops(distance, start, destination),
+            3
+        );
     }
 
     {
         let distance = vec![1, 2, 3, 4];
         let start = 0;
         let destination = 3;
-        assert_eq!(distance_between_bus_stops(distance, start, destination), 4);
+        assert_eq!(
+            Solution::distance_between_bus_stops(distance, start, destination),
+            4
+        );
     }
 }
