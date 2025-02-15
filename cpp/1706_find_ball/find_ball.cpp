d3e8cf07c8a62e2457bad1092a9134aa43b87ad4
mengsen wang <854284842@qq.com>
2025-02-15 10:22:04 +0800

    2025年2月15日

--- cpp/1706_find_ball/find_ball.cpp
+++ cpp/1706_find_ball/find_ball.cpp
@@ -1,12 +1,5 @@
-/*
- * @Date: 2022-02-23 23:51:44
- * @Author: Mengsen Wang
- * @LastEditors: Mengsen Wang
- * @LastEditTime: 2022-02-24 00:14:34
- * @FilePath: /algorithm/1706_find_ball/find_ball.cpp
- */
-
 #include <cassert>
+#include <tuple>
 #include <vector>
 
 using namespace std;
@@ -35,28 +28,15 @@ class Solution {
 };
 
 int main() {
-  {
-    vector<vector<int>> grid{{1, 1, 1, -1, -1},
-                             {1, 1, 1, -1, -1},
-                             {-1, -1, -1, 1, 1},
-                             {1, 1, 1, 1, -1},
-                             {-1, -1, -1, -1, -1}};
-    vector<int> ans{1, -1, -1, -1, -1};
-    assert(Solution().findBall(grid) == ans);
-  }
-
-  {
-    vector<vector<int>> grid{{-1}};
-    vector<int> ans{-1};
-    assert(Solution().findBall(grid) == ans);
-  }
-
-  {
-    vector<vector<int>> grid{{1, 1, 1, 1, 1, 1},
-                             {-1, -1, -1, -1, -1, -1},
-                             {1, 1, 1, 1, 1, 1},
-                             {-1, -1, -1, -1, -1, -1}};
-    vector<int> ans{0, 1, 2, 3, 4, -1};
+  vector<tuple<vector<vector<int>>, vector<int>>> tests{
+      {{{1, 1, 1, -1, -1}, {1, 1, 1, -1, -1}, {-1, -1, -1, 1, 1}, {1, 1, 1, 1, -1}, {-1, -1, -1, -1, -1}},
+       {1, -1, -1, -1, -1}},
+      {{{-1}}, {-1}},
+      {{{1, 1, 1, 1, 1, 1}, {-1, -1, -1, -1, -1, -1}, {1, 1, 1, 1, 1, 1}, {-1, -1, -1, -1, -1, -1}},
+       {0, 1, 2, 3, 4, -1}},
+  };
+  
+  for (auto &[grid, ans] : tests) {
     assert(Solution().findBall(grid) == ans);
   }
 }
