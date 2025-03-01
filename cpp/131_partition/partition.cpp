9c8ce2779abdb052990b5ebed91031f9371f8514
mengsen wang <854284842@qq.com>
2025-03-01 19:30:01 +0800

    2025年3月1日

--- cpp/131_partition/partition.cpp
+++ cpp/131_partition/partition.cpp
@@ -1,45 +1,54 @@
-/*
- * @Date: 2021-03-07 09:30:21
- * @Author: mengsen
- * @LastEditors: mengsen
- * @LastEditTime: 2021-03-07 09:44:02
- */
-
 #include <cassert>
 #include <string>
+#include <tuple>
 #include <vector>
 
 using namespace std;
 
-vector<vector<int>> hw;
-vector<vector<string>> res;
-vector<string> tmp;
-void dfs(int x, string &s, int &n) {
-  if (x == n) res.push_back(tmp);
-  for (int i = 0; i < hw[x].size(); i++) {
-    tmp.push_back(s.substr(x, hw[x][i]));
-    dfs(x + hw[x][i], s, n);
-    tmp.pop_back();
-  }
-}
+class Solution {
+ private:
+  vector<vector<int>> f;
+  vector<vector<string>> ret;
+  vector<string> ans;
+  int n;
 
-vector<vector<string>> partition(string &&s) {
-  int n = s.size();
-  hw.resize(n + 1);
-  for (int i = n - 1; i >= 0; i--) {
-    hw[i].push_back(1);
-    if (i + 1 < n && s[i] == s[i + 1]) hw[i].push_back(2);
-    for (int j = 0; j < hw[i + 1].size(); j++) {
-      int index = i + hw[i + 1][j] + 1;
-      if (index < n && s[i] == s[index]) hw[i].push_back(index - i + 1);
+ public:
+  void dfs(const string &s, int i) {
+    if (i == n) {
+      ret.push_back(ans);
+      return;
+    }
+    for (int j = i; j < n; ++j) {
+      if (f[i][j]) {
+        ans.push_back(s.substr(i, j - i + 1));
+        dfs(s, j + 1);
+        ans.pop_back();
+      }
     }
   }
-  dfs(0, s, n);
-  return res;
-}
+
+  vector<vector<string>> partition(string s) {
+    n = s.size();
+    f.assign(n, vector<int>(n, true));
+
+    for (int i = n - 1; i >= 0; --i) {
+      for (int j = i + 1; j < n; ++j) {
+        f[i][j] = (s[i] == s[j]) && f[i + 1][j - 1];
+      }
+    }
+
+    dfs(s, 0);
+    return ret;
+  }
+};
 
 int main() {
-  assert(partition(string("aab")) ==
-         move(vector<vector<string>>{{"a", "a", "b"}, {"aa", "b"}}));
-  return 0;
+  vector<tuple<string, vector<vector<string>>>> tests{
+      {"aab", {{"a", "a", "b"}, {"aa", "b"}}},
+      {"a", {{"a"}}},
+  };
+  
+  for (auto &[s, ans] : tests) {
+    assert(Solution().partition(s) == ans);
+  }
 }
--- rust/131_partition/partition.rs
+++ rust/131_partition/partition.rs
@@ -5,42 +5,56 @@
  * @LastEditTime: 2021-03-07 09:49:33
  */
 
-fn partition(s: String) -> Vec<Vec<String>> {
-    fn is_palindrome(s: &[u8], lo: usize, hi: usize) -> bool {
-        let mut i = lo;
-        let mut j = hi;
-        while i < j {
-            if s[i] != s[j] {
-                return false;
-            }
-            i += 1;
-            j -= 1;
-        }
-        true
-    }
+struct Solution;
 
-    fn dfs(arr: &mut Vec<String>, s: &str, i: usize, answer: &mut Vec<Vec<String>>) {
-        if i == s.len() {
-            answer.push(arr.clone());
-            return;
+impl Solution {
+    pub fn partition(s: String) -> Vec<Vec<String>> {
+        fn is_palindrome(s: &[u8], lo: usize, hi: usize) -> bool {
+            let mut i = lo;
+            let mut j = hi;
+            while i < j {
+                if s[i] != s[j] {
+                    return false;
+                }
+                i += 1;
+                j -= 1;
+            }
+            true
         }
-        for j in i..s.len() {
-            if is_palindrome(s.as_bytes(), i, j) {
-                arr.push(s[i..=j].to_string());
-                dfs(arr, s, j + 1, answer);
-                arr.pop();
+
+        fn dfs(arr: &mut Vec<String>, s: &str, i: usize, answer: &mut Vec<Vec<String>>) {
+            if i == s.len() {
+                answer.push(arr.clone());
+                return;
+            }
+            for j in i..s.len() {
+                if is_palindrome(s.as_bytes(), i, j) {
+                    arr.push(s[i..=j].to_string());
+                    dfs(arr, s, j + 1, answer);
+                    arr.pop();
+                }
             }
         }
-    }
 
-    let mut answer = Vec::new();
-    dfs(&mut Vec::new(), &s, 0, &mut answer);
-    answer
+        let mut answer = Vec::new();
+        dfs(&mut Vec::new(), &s, 0, &mut answer);
+        answer
+    }
 }
 
 fn main() {
-    assert_eq!(
-        partition("aab".to_string()),
-        vec![vec!["a", "a", "b"], vec!["aa", "b"]]
-    );
+    let tests = vec![
+        (
+            "aab".to_string(),
+            vec![
+                vec!["a".to_string(), "a".to_string(), "b".to_string()],
+                vec!["aa".to_string(), "b".to_string()],
+            ],
+        ),
+        ("a".to_string(), vec![vec!["a".to_string()]]),
+    ];
+
+    for (s, ans) in tests {
+        assert_eq!(Solution::partition(s), ans);
+    }
 }
