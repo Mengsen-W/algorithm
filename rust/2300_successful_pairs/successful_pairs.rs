82fb3bebb06213eb6c1a2580f0c331ac45e7f265
mengsenwang <mengsenwang@tencent.com>
2025-10-08 05:22:13 +0800

    2025年10月8日

--- rust/2300_successful_pairs/successful_pairs.rs
+++ rust/2300_successful_pairs/successful_pairs.rs
@@ -1,10 +1,3 @@
-/*
- * @Date: 2023-11-10
- * @LastEditors: 854284842@qq.com
- * @LastEditTime: 2023-11-10
- * @FilePath: /algorithm/rust/2300_successful_pairs/successful_pairs.rs
- */
-
 struct Solution;
 
 impl Solution {
