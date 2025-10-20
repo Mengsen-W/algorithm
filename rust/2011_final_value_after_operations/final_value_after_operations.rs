897a29f5f8850017ba019586f0c7cacd2ae12033
mengsenwang <854284842@qq.com>
2025-10-20 10:36:57 +0800

    2025年10月20日

--- rust/2011_final_value_after_operations/final_value_after_operations.rs
+++ rust/2011_final_value_after_operations/final_value_after_operations.rs
@@ -1,45 +1,30 @@
-/*
- * @Date: 2022-12-23
- * @LastEditors: mengsen_wang@163.com
- * @LastEditTime: 2022-12-23
- * @FilePath: /algorithm/2011_final_value_after_operations/final_value_after_operations.rs
- */
+struct Solution;
 
-pub fn final_value_after_operations(operations: Vec<String>) -> i32 {
-    operations.iter().fold(0, |mut x: i32, op: &String| {
-        match op.chars().nth(1).unwrap() {
-            '-' => x -= 1,
-            _ => x += 1,
-        }
-        x
-    })
+impl Solution {
+    pub fn final_value_after_operations(operations: Vec<String>) -> i32 {
+        operations.iter().fold(0, |mut x: i32, op: &String| {
+            match op.chars().nth(1).unwrap() {
+                '-' => x -= 1,
+                _ => x += 1,
+            }
+            x
+        })
+    }
 }
 
 fn main() {
-    {
-        let operations = vec!["--X", "X++", "X++"]
-            .iter()
-            .map(|s| s.to_string())
-            .collect();
-        let ans = 1;
-        assert_eq!(final_value_after_operations(operations), ans);
-    }
+    let tests = vec![
+        (vec!["--X", "X++", "X++"], 1),
+        (vec!["++X", "++X", "X++"], 3),
+        (vec!["X++", "++X", "--X", "X--"], 0),
+    ];
 
-    {
-        let operations = vec!["++X", "++X", "X++"]
-            .iter()
-            .map(|s| s.to_string())
-            .collect();
-        let ans = 3;
-        assert_eq!(final_value_after_operations(operations), ans);
-    }
-
-    {
-        let operations = vec!["X++", "++X", "--X", "X--"]
-            .iter()
-            .map(|s| s.to_string())
-            .collect();
-        let ans = 0;
-        assert_eq!(final_value_after_operations(operations), ans);
+    for (operations, ans) in tests {
+        assert_eq!(
+            Solution::final_value_after_operations(
+                operations.iter().map(|s| s.to_string()).collect()
+            ),
+            ans
+        );
     }
 }
