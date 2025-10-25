14593dbb054d5ae5a675e732cb5e45b747508a9d
mengsenwang <854284842@qq.com>
2025-10-25 16:29:05 +0800

    2025年10月25日

--- rust/1716_total_money/total_money.rs
+++ rust/1716_total_money/total_money.rs
@@ -1,24 +1,23 @@
-/*
- * @Date: 2022-01-15 01:43:32
- * @Author: Mengsen Wang
- * @LastEditors: Mengsen Wang
- * @LastEditTime: 2022-01-15 02:12:18
- */
+struct Solution;
 
-pub fn total_money(n: i32) -> i32 {
-    let week_number = n / 7;
-    let first_week_money = (1 + 7) * 7 / 2;
-    let last_week_money = first_week_money + 7 * (week_number - 1);
-    let week_money = (first_week_money + last_week_money) * week_number / 2;
-    let day_number = n % 7;
-    let first_day_money = 1 + week_number;
-    let last_day_money = first_day_money + day_number - 1;
-    let day_money = (first_day_money + last_day_money) * day_number / 2;
-    week_money + day_money
+impl Solution {
+    pub fn total_money(n: i32) -> i32 {
+        let week_number = n / 7;
+        let first_week_money = (1 + 7) * 7 / 2;
+        let last_week_money = first_week_money + 7 * (week_number - 1);
+        let week_money = (first_week_money + last_week_money) * week_number / 2;
+        let day_number = n % 7;
+        let first_day_money = 1 + week_number;
+        let last_day_money = first_day_money + day_number - 1;
+        let day_money = (first_day_money + last_day_money) * day_number / 2;
+        week_money + day_money
+    }
 }
 
 fn main() {
-    assert_eq!(total_money(4), 10);
-    assert_eq!(total_money(10), 37);
-    assert_eq!(total_money(20), 96);
+    let tests = vec![(4, 10), (10, 37), (20, 96)];
+
+    for (n, expected) in tests {
+        assert_eq!(Solution::total_money(n), expected);
+    }
 }
