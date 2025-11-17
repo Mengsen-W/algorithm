76b5447c8a29520c96f04c534121fd6465c55b35
mengsenwang <854284842@qq.com>
2025-11-17 09:41:54 +0800

    2025年11月17日

--- rust/1437_k_length_apart/k_length_apart.rs
+++ rust/1437_k_length_apart/k_length_apart.rs
@@ -1,33 +1,29 @@
-/*
- * @Author: Mengsen.Wang
- * @Date: 2021-02-17 23:36:00
- * @Last Modified by: Mengsen.Wang
- * @Last Modified time: 2021-02-17 23:40:53
- */
+struct Solution;
 
-fn k_length_apart(nums: Vec<i32>, k: i32) -> bool {
-    let k = k as usize;
-    let mut dst = i32::MAX as usize;
-    for i in (0..nums.len()).rev() {
-        if nums[i] == 1 {
-            if dst - i <= k {
-                return false;
-            } else {
-                dst = i
+impl Solution {
+    pub fn k_length_apart(nums: Vec<i32>, k: i32) -> bool {
+        let k = k as usize;
+        let mut dst = i32::MAX as usize;
+        for i in (0..nums.len()).rev() {
+            if nums[i] == 1 {
+                if dst - i <= k {
+                    return false;
+                } else {
+                    dst = i
+                }
             }
         }
+        true
     }
-    true
 }
 
 fn main() {
-    let mut nums: Vec<i32>;
-    nums = vec![1, 0, 0, 0, 1, 0, 0, 1];
-    println!("{}", k_length_apart(nums, 2));
-    nums = vec![1, 0, 0, 1, 0, 1];
-    println!("{}", k_length_apart(nums, 2));
-    nums = vec![1, 1, 1, 1, 1];
-    println!("{}", k_length_apart(nums, 0));
-    nums = vec![0, 1, 0, 1];
-    println!("{}", k_length_apart(nums, 0));
+    let tests = vec![
+        (vec![1, 0, 0, 0, 1, 0, 0, 1], 2, true),
+        (vec![1, 0, 0, 1, 0, 1], 2, false),
+    ];
+
+    for (nums, k, expected) in tests {
+        assert_eq!(Solution::k_length_apart(nums, k), expected);
+    }
 }
